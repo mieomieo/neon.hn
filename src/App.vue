3ea7f1f20a7974838197cd188b0d16b4fd271d82
mieomieo <mieo.mieo.1102@gmail.com>
2023-02-17 16:20:32 +0700

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,53 @@
 <template>
   <div class="w-3/4 mx-auto text-center flex justify-between">
+    <ul
+      class="nav nav-tabs flex flex-col md:flex-row flex-wrap list-none border-b-0 pl-0 mb-4"
+      id="tabs-tab"
+      role="tablist"
+    >
+      <li class="nav-item" role="presentation">
+        <a
+          href="#tabs-home"
+          class="nav-link block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent active"
+          id="tabs-home-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#tabs-home"
+          role="tab"
+          aria-controls="tabs-home"
+          aria-selected="true"
+          >Text</a
+        >
+      </li>
+      <li class="nav-item" role="presentation">
+        <a
+          href="#tabs-profile"
+          class="nav-link block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent"
+          id="tabs-profile-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#tabs-profile"
+          role="tab"
+          aria-controls="tabs-profile"
+          aria-selected="false"
+          >Font</a
+        >
+      </li>
+      <li class="nav-item" role="presentation">
+        <a
+          href="#tabs-messages"
+          class="nav-link block font-medium text-xs leading-tight uppercase border-x-0 border-t-0 border-b-2 border-transparent px-6 py-3 my-2 hover:border-transparent hover:bg-gray-100 focus:border-transparent"
+          id="tabs-messages-tab"
+          data-bs-toggle="pill"
+          data-bs-target="#tabs-messages"
+          role="tab"
+          aria-controls="tabs-messages"
+          aria-selected="false"
+          >COLOR</a
+        >
+      </li>
+    </ul>
+
     <ChooseText :lightOn="isActiveLight" />
     <ShowImage @toggleSwitch="handleSwitch" />
-    <!-- <HedaerApp /> -->
   </div>
 </template>
 <script>
@@ -14,6 +59,7 @@ export default {
     ShowImage,
     ChooseText,
   },
+
   data() {
     return {
       isActiveLight: true,
